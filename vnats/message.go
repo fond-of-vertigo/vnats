65df4ebd0f8f338b262d2d5180cccfdb34868cef
Leander Kohler <leander.kohler@fondof.de>
2022-03-21 10:59:31 +0100

    Add message docstrings

--- vnats/message.go
+++ vnats/message.go
@@ -3,9 +3,15 @@ package vnats
 import "github.com/nats-io/nats.go"
 
 type Message interface {
+	// AckSync acknowledges successful Message processing.
 	AckSync() error
+	// InProgress tells Server that the Message processing is still ongoing.
+	InProgress() error
+	// Data Returns payload Data of this Message.
 	Data() []byte
+	// Subject under which this Message is sent.
 	Subject() string
+	// MsgID for de-duplication relative to the duplication-time-window of each streamInfo.
 	MsgID() string
 }
 
@@ -17,9 +23,14 @@ func (m *natsMsg) AckSync() error {
 	return m.msg.AckSync()
 }
 
+func (m *natsMsg) InProgress() error {
+	return m.msg.InProgress()
+}
+
 func (m *natsMsg) Data() []byte {
 	return m.msg.Data
 }
+
 func (m *natsMsg) MsgID() string {
 	return m.msg.Header.Get("Nats-Msg-Id")
 }
