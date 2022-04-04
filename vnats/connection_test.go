4b7fb4122bde524ba1d5a534c64a9d2d137224e8
Leander Kohler <leander.kohler@fondof.de>
2022-04-04 15:07:31 +0200

    Add basic integration tests for new Pub/ Sub

--- vnats/connection_test.go
+++ vnats/connection_test.go
@@ -16,3 +16,49 @@ func TestConnect(t *testing.T) {
 		t.Errorf("NATS connection could not be established: %v", err)
 	}
 }
+
+func TestConnection_NewPublisher(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+	conn, err := Connect([]string{"127.0.0.1:4222"}, log)
+	if err != nil {
+		t.Errorf("NATS connection could not be established: %v", err)
+	}
+
+	_, err = conn.NewPublisher("IntegrationTests")
+	if err != nil {
+		t.Errorf("Publisher could not be created: %v", err)
+	}
+}
+
+type newSubscriberConfig struct {
+	consumerName string
+	subject      string
+	mode         SubscriptionMode
+}
+
+var newSubscriberTestCases = []newSubscriberConfig{
+	{"IntegrationTestConsumer", "IntegrationTests.*", MultipleInstances},
+	{"IntegrationTestConsumer", "IntegrationTests.*", SingleInstanceMessagesInOrder},
+	{"IntegrationTestConsumer", "IntegrationTests.tests.*", MultipleInstances},
+	{"IntegrationTestConsumer", "IntegrationTests.tests.*", SingleInstanceMessagesInOrder},
+}
+
+func TestConnection_NewSubscriber(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+	conn, err := Connect([]string{"127.0.0.1:4222"}, log)
+	if err != nil {
+		t.Errorf("NATS connection could not be established: %v", err)
+	}
+
+	for _, test := range newSubscriberTestCases {
+		_, err = conn.NewSubscriber(test.consumerName, test.subject, test.mode)
+		if err != nil {
+			t.Errorf("Subscriber could not be created: %v", err)
+		}
+	}
+
+}
